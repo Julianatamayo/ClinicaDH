27c55b133ce964e57752e0f531572f01f1e67a45
julianatmayo23 <julianatmayo23@gmail.com>
2024-09-12 20:21:25 -0500

    commit JulianaT

--- src/main/java/com/clinica/dh/repository/IDomicilioRepository.java
+++ src/main/java/com/clinica/dh/repository/IDomicilioRepository.java
@@ -1,4 +1,10 @@
 package com.clinica.dh.repository;
 
-public interface IDomicilioRepository {
+
+import com.dh.clinica.entity.Domicilio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IDomicilioRepository extends JpaRepository <Domicilio, Integer>{
 }
--- src/main/java/com/clinica/dh/repository/IOdontologoRepository.java
+++ src/main/java/com/clinica/dh/repository/IOdontologoRepository.java
@@ -1,4 +1,15 @@
-package com.clinica.dh.repository;
+import com.dh.clinica.entity.Odontologo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-public interface IOdontologoRepository {
-}
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface IOdontologoRepository extends JpaRepository<Odontologo, Integer> {
+    List<Odontologo> findByApellidoAndNombre(String apellido, String nombre);
+
+    @Query("Select p from Odontologo p where p.nombre LIKE %:parteNombre% ")
+    List<Odontologo> findByNombreLike(String parteNombre);
+    Optional<Odontologo> findByMatriculaLike(String parteMatricula);
