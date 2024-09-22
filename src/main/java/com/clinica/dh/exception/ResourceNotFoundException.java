8b987885a1451bdd3f4c250beed40bfd0bce6f18
julianatmayo23 <julianatmayo23@gmail.com>
2024-09-22 15:13:48 -0500

    correccion de trabajo

--- src/main/java/com/clinica/dh/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.clinica.dh.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
